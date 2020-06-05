033f75c5cf9f1373522573d886d3bd92a1f0df92
Nikita Korobov <ge75muw@mytum.de>
2020-06-05 16:51:02 +0200

    format

--- run_format.sh
+++ run_format.sh
@@ -3,5 +3,5 @@
 set -e
 
 find ./tools -iname "*.hpp" -or -iname "*.h" -or -iname "*.cpp" | xargs clang-format -i
-find ./implementation -iname "*.hpp" -or -iname "*.h" -or -iname "*.cpp" | xargs clang-format -i
 find ./common -iname "*.hpp" -or -iname "*.h" -or -iname "*.cpp" | xargs clang-format -i
+find ./implementation -iname "*.hpp" -or -iname "*.h" -or -iname "*.cpp" | xargs clang-format -i
